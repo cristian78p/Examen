9ebe363423c1a0b037e1a59f9c2288ffbad6c08d
cristian78p <ccreyb@gmail.com>
2025-03-06 18:08:50 -0500

    Clase producto agregada

---
+++ Examen/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public Producto(string nombre, decimal precio, int cantidad) 
+        {
+            Nombre = nombre;
+            Precio = precio;
+            Cantidad = cantidad;
+        }
+    }
+}
