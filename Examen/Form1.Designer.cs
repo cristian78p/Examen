3ba5095bad710b33d8b8cb5fb1c22e197be0534e
cristian78p <ccreyb@gmail.com>
2025-03-06 22:05:21 -0500

    codigo main realizado

--- Examen/Form1.Designer.cs
+++ Examen/Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.btnAgregarProducto = new System.Windows.Forms.Button();
             this.txtPrecioProducto = new System.Windows.Forms.TextBox();
             this.txtCantidadStock = new System.Windows.Forms.TextBox();
-            this.listBoxInventario = new System.Windows.Forms.DataGridView();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -46,6 +45,7 @@
             this.btnVenderProducto = new System.Windows.Forms.Button();
             this.txtBuscarProducto = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.listBoxInventario = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.listBoxInventario)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,7 +53,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 219);
+            this.label1.Location = new System.Drawing.Point(17, 172);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(143, 15);
             this.label1.TabIndex = 0;
@@ -61,47 +61,40 @@
             // 
             // txtNombreProducto
             // 
-            this.txtNombreProducto.Location = new System.Drawing.Point(15, 237);
+            this.txtNombreProducto.Location = new System.Drawing.Point(20, 190);
             this.txtNombreProducto.Name = "txtNombreProducto";
             this.txtNombreProducto.Size = new System.Drawing.Size(200, 20);
             this.txtNombreProducto.TabIndex = 1;
             // 
             // btnAgregarProducto
             // 
-            this.btnAgregarProducto.Location = new System.Drawing.Point(15, 383);
+            this.btnAgregarProducto.Location = new System.Drawing.Point(20, 336);
             this.btnAgregarProducto.Name = "btnAgregarProducto";
             this.btnAgregarProducto.Size = new System.Drawing.Size(140, 39);
             this.btnAgregarProducto.TabIndex = 2;
             this.btnAgregarProducto.Text = "Agregar producto";
             this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            this.btnAgregarProducto.Click += new System.EventHandler(this.btnAgregarProducto_Click);
             // 
             // txtPrecioProducto
             // 
-            this.txtPrecioProducto.Location = new System.Drawing.Point(15, 288);
+            this.txtPrecioProducto.Location = new System.Drawing.Point(20, 241);
             this.txtPrecioProducto.Name = "txtPrecioProducto";
             this.txtPrecioProducto.Size = new System.Drawing.Size(200, 20);
             this.txtPrecioProducto.TabIndex = 5;
             // 
             // txtCantidadStock
             // 
-            this.txtCantidadStock.Location = new System.Drawing.Point(15, 339);
+            this.txtCantidadStock.Location = new System.Drawing.Point(20, 292);
             this.txtCantidadStock.Name = "txtCantidadStock";
             this.txtCantidadStock.Size = new System.Drawing.Size(200, 20);
             this.txtCantidadStock.TabIndex = 6;
-            // 
-            // listBoxInventario
-            // 
-            this.listBoxInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.listBoxInventario.Location = new System.Drawing.Point(265, 131);
-            this.listBoxInventario.Name = "listBoxInventario";
-            this.listBoxInventario.Size = new System.Drawing.Size(390, 413);
-            this.listBoxInventario.TabIndex = 14;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(11, 131);
+            this.label7.Location = new System.Drawing.Point(16, 84);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(230, 24);
             this.label7.TabIndex = 15;
@@ -111,7 +104,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(686, 131);
+            this.label8.Location = new System.Drawing.Point(622, 84);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(215, 24);
             this.label8.TabIndex = 16;
@@ -121,7 +114,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 270);
+            this.label4.Location = new System.Drawing.Point(17, 223);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 15);
             this.label4.TabIndex = 17;
@@ -131,7 +124,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 321);
+            this.label2.Location = new System.Drawing.Point(17, 274);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(148, 15);
             this.label2.TabIndex = 18;
@@ -141,7 +134,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(389, 25);
+            this.label3.Location = new System.Drawing.Point(359, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(135, 24);
             this.label3.TabIndex = 19;
@@ -151,7 +144,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(687, 321);
+            this.label5.Location = new System.Drawing.Point(623, 274);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(123, 15);
             this.label5.TabIndex = 26;
@@ -161,7 +154,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(687, 270);
+            this.label6.Location = new System.Drawing.Point(623, 223);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(153, 15);
             this.label6.TabIndex = 25;
@@ -169,50 +162,63 @@
             // 
             // txtCantidadVenta
             // 
-            this.txtCantidadVenta.Location = new System.Drawing.Point(690, 339);
+            this.txtCantidadVenta.Location = new System.Drawing.Point(626, 292);
             this.txtCantidadVenta.Name = "txtCantidadVenta";
             this.txtCantidadVenta.Size = new System.Drawing.Size(200, 20);
             this.txtCantidadVenta.TabIndex = 24;
             // 
             // txtProductoSeleccionado
             // 
-            this.txtProductoSeleccionado.Location = new System.Drawing.Point(690, 288);
+            this.txtProductoSeleccionado.Location = new System.Drawing.Point(626, 241);
             this.txtProductoSeleccionado.Name = "txtProductoSeleccionado";
+            this.txtProductoSeleccionado.ReadOnly = true;
             this.txtProductoSeleccionado.Size = new System.Drawing.Size(200, 20);
             this.txtProductoSeleccionado.TabIndex = 23;
             this.txtProductoSeleccionado.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // btnVenderProducto
             // 
-            this.btnVenderProducto.Location = new System.Drawing.Point(690, 383);
+            this.btnVenderProducto.Location = new System.Drawing.Point(626, 336);
             this.btnVenderProducto.Name = "btnVenderProducto";
             this.btnVenderProducto.Size = new System.Drawing.Size(140, 39);
             this.btnVenderProducto.TabIndex = 22;
             this.btnVenderProducto.Text = "Vender Producto";
             this.btnVenderProducto.UseVisualStyleBackColor = true;
+            this.btnVenderProducto.Click += new System.EventHandler(this.btnVenderProducto_Click);
             // 
             // txtBuscarProducto
             // 
-            this.txtBuscarProducto.Location = new System.Drawing.Point(265, 94);
+            this.txtBuscarProducto.Location = new System.Drawing.Point(270, 84);
             this.txtBuscarProducto.Name = "txtBuscarProducto";
-            this.txtBuscarProducto.Size = new System.Drawing.Size(390, 20);
+            this.txtBuscarProducto.Size = new System.Drawing.Size(329, 20);
             this.txtBuscarProducto.TabIndex = 21;
+            this.txtBuscarProducto.TextChanged += new System.EventHandler(this.txtBuscarProducto_TextChanged);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(400, 76);
+            this.label9.Location = new System.Drawing.Point(370, 66);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(111, 15);
             this.label9.TabIndex = 20;
             this.label9.Text = "Buscar producto";
             // 
+            // listBoxInventario
+            // 
+            this.listBoxInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listBoxInventario.Location = new System.Drawing.Point(270, 111);
+            this.listBoxInventario.Name = "listBoxInventario";
+            this.listBoxInventario.Size = new System.Drawing.Size(329, 376);
+            this.listBoxInventario.TabIndex = 27;
+            this.listBoxInventario.SelectionChanged += new System.EventHandler(this.listBoxInventario_SelectionChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(956, 577);
+            this.ClientSize = new System.Drawing.Size(875, 518);
+            this.Controls.Add(this.listBoxInventario);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtCantidadVenta);
@@ -225,7 +231,6 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.listBoxInventario);
             this.Controls.Add(this.txtCantidadStock);
             this.Controls.Add(this.txtPrecioProducto);
             this.Controls.Add(this.btnAgregarProducto);
@@ -246,7 +251,6 @@
         private System.Windows.Forms.Button btnAgregarProducto;
         private System.Windows.Forms.TextBox txtPrecioProducto;
         private System.Windows.Forms.TextBox txtCantidadStock;
-        private System.Windows.Forms.DataGridView listBoxInventario;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label4;
@@ -259,6 +263,7 @@
         private System.Windows.Forms.Button btnVenderProducto;
         private System.Windows.Forms.TextBox txtBuscarProducto;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DataGridView listBoxInventario;
     }
 }
 
--- Examen/Form1.cs
+++ Examen/Form1.cs
@@ -12,14 +12,151 @@ namespace Examen
 {
     public partial class Form1 : Form
     {
+        List<Producto> productos;
         public Form1()
         {
             InitializeComponent();
+            productos = new List<Producto>();
         }
 
         private void textBox5_TextChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void btnAgregarProducto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombreProducto.Text))
+                {
+                    MessageBox.Show("El nombre del producto no puede estar vacío.", "Error");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtPrecioProducto.Text))
+                {
+                    MessageBox.Show("El precio del producto no puede estar vacío.", "Error");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtCantidadStock.Text))
+                {
+                    MessageBox.Show("La cantidad en stock no puede estar vacía.", "Error");
+                    return;
+                }
+
+                if (!decimal.TryParse(txtPrecioProducto.Text, out decimal precio) || precio < 0)
+                {
+                    MessageBox.Show("El precio debe ser un número válido y mayor o igual a 0.", "Error");
+                    return;
+                }
+
+                if (!int.TryParse(txtCantidadStock.Text, out int cantidad) || cantidad < 0)
+                {
+                    MessageBox.Show("La cantidad debe ser un número entero válido y mayor o igual a 0.", "Error");
+                    return;
+                }
+                Producto producto = new Producto(txtNombreProducto.Text,precio,cantidad);
+                productos.Add(producto);
+                ActualizarInventario();
+                MessageBox.Show("Producto agregado exitosamente.", "Éxito");
+                limpiarAgrProducto();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("ingrese datos validos");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void txtBuscarProducto_TextChanged(object sender, EventArgs e)
+        {
+            ActualizarInventario(txtBuscarProducto.Text) ;
+        }
+
+        private void listBoxInventario_SelectionChanged(object sender, EventArgs e)
+        {
+            if (listBoxInventario.SelectedRows.Count > 0)
+            {
+                Producto productoSeleccionado = (Producto)listBoxInventario.SelectedRows[0].DataBoundItem;
+                txtProductoSeleccionado.Text = productoSeleccionado.Nombre;
+            }
+        }
+
+        private void btnVenderProducto_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtProductoSeleccionado.Text))
+            {
+                MessageBox.Show("Debe seleccionar un producto a vender.", "Error");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCantidadVenta.Text))
+            {
+                MessageBox.Show("La cantidad a vender no puede estar vacía.", "Error");
+                return;
+            }
+
+            if (!int.TryParse(txtCantidadVenta.Text, out int cantidadVenta) || cantidadVenta <= 0)
+            {
+                MessageBox.Show("La cantidad a vender debe ser un número entero mayor a 0.", "Error");
+                return;
+            }
+
+            Producto producto = productos.Find(p => p.Nombre.Equals(txtProductoSeleccionado.Text, StringComparison.OrdinalIgnoreCase));
+            if (producto == null)
+            {
+                MessageBox.Show("El producto no se encuentra en el inventario.", "Error");
+                return;
+            }
+
+            if (producto.Cantidad < cantidadVenta)
+            {
+                MessageBox.Show("No hay suficiente stock para realizar la venta.", "Error");
+                return;
+            }
+
+            producto.Cantidad -= cantidadVenta;
+
+            ActualizarInventario();
+            limpiarVenderProducto();    
+
+            MessageBox.Show("Venta realizada exitosamente.", "Éxito");
+        }
+        private void limpiarAgrProducto()
+        {
+            txtNombreProducto.Clear();
+            txtCantidadStock.Clear();
+            txtPrecioProducto.Clear();
+        }
+
+        private void limpiarVenderProducto()
+        {
+            txtCantidadVenta.Clear();
+            txtBuscarProducto.Clear();
+        }
+        private void ActualizarInventario(string filtro ="")
+        {
+            if (string.IsNullOrWhiteSpace(filtro))
+            {
+                listBoxInventario.DataSource = null;
+                listBoxInventario.DataSource = productos;
+            }
+            else
+            {
+                var productosFiltrados = productos
+                    .Where(p => p.Nombre.Contains(filtro))
+                    .ToList();
+                listBoxInventario.DataSource = null;
+                listBoxInventario.DataSource = productosFiltrados;
+            }
+
+        }
+
     }
 }
